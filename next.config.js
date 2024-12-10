7fb18534cbb32bd78f3cbe1c8710de065ca12393
TackTeam-Chare <chare.auppachitwan@gmail.com>
2024-12-10 17:06:55 +0700

    add .

--- next.config.js
+++ next.config.js
@@ -2,26 +2,7 @@ module.exports = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_LIFF_ID: process.env.NEXT_PUBLIC_LIFF_ID,
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: '/src/app/api/:path*',
-      },
-    ];
-  },
-    async headers() {
-      return [
-        {
-          source: "/api/(.*)",
-          headers: [
-            { key: "Access-Control-Allow-Origin", value: "*" },
-            { key: "Access-Control-Allow-Methods", value: "POST,GET,OPTIONS" },
-            { key: "Access-Control-Allow-Headers", value: "Content-Type" },
-          ],
-        },
-      ];
-    },
   };
   
